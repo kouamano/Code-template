fa3e2bf81e41fb300a56fd573e8510ab2f94fcee
kouamano <amano.au1@gmail.com>
2013-12-04 14:46:12 +0900

    add

--- C/template.main.c
+++ C/template.main.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define LEN 1024
 
 struct options {
+	int argint;
+	char *argstr;
 };
 
 void help(void){
 	printf("USAGE:\n");
+}
+
+void alloc_options(void){
+	struct options *p;
+	if((p = malloc(sizeof(struct options) * 1)) == UNLL){
+		printf("failed : malloc() in alloc_options().\n");
+		exit(1);
+	}
+	if(((*p).argstr = malloc(sizeof(char) * LEN)) == NULL){
+		printf("failed : malloc() in alloc_options().\n");
+		exit(1);
+	}
 }
 
 void init_options(struct options *opt){
