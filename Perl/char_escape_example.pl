8279258d2676546f34df02e4f5803f8809f514bd
kouamano <amano.au1@gmail.com>
2015-07-14 17:19:32 +0900

    new

---
+++ Perl/char_escape_example.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+
+while(<>){
+	push(@tmp,$_);
+}
+
+@tmp2 = &escape(@tmp);
+
+foreach(@tmp2){
+	print $_;
+}
+
+sub escape {
+	my @all = ();
+	my @out = ();
+	my @sp = ();
+	my $out = "";
+	foreach(@_){
+		@out = ();
+		@sp = split(//,$_);
+                foreach(@sp){
+			if($_ eq '\\'){
+				push(@out,"\\\\");
+                        }elsif($_ eq '*'){
+                                push(@out,"\\\*");
+                        }elsif($_ eq '+'){
+                                push(@out,"\\\+");
+                        }elsif($_ eq '.'){
+                                push(@out,"\\\.");
+                        }elsif($_ eq '?'){
+                                push(@out,"\\\?");
+                        }elsif($_ eq '{'){
+                                push(@out,"\\\{");
+                        }elsif($_ eq '}'){
+                                push(@out,"\\\}");
+                        }elsif($_ eq '('){
+                                push(@out,"\\\(");
+                        }elsif($_ eq ')'){
+                                push(@out,"\\\)");
+                        }elsif($_ eq '['){
+                                push(@out,"\\\[");
+                        }elsif($_ eq ']'){
+                                push(@out,"\\\]");
+                        }elsif($_ eq '^'){
+                                push(@out,"\\\^");
+                        }elsif($_ eq '$'){
+                                push(@out,"\\\$");
+                        }elsif($_ eq '|'){
+                                push(@out,"\\\|");
+                        }elsif($_ eq '/'){
+                                push(@out,"\\\/");
+                        }else{
+				push(@out,"$_");
+			}
+                }
+		$out = join("",@out);
+		push(@all,$out);
+        }
+	return @all;
+}
